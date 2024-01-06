f6bd7c93ae1c24f9b38708b5ea0bc8a172f56881
github-username <dzhumadylov.ch@gmail.com>
2024-01-06 18:31:29 +0600

    refactor: update index.html & style.css according to Media alternatives to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement = function (taskString) {
     deleteButton.className = "button delete";
     deleteButtonImg.src = './remove.svg';
     deleteButtonImg.classList.add('delete-icon');
+    deleteButtonImg.setAttribute('alt', 'remove icon');
     deleteButton.appendChild(deleteButtonImg);
 
 
